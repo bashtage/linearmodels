dfe9b6fcc302547b54b5e079b4d6953b03486dd7
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-03-18 23:31:12 +0000

    REF: Refactor PanelDataHandler
    Refactor panel data hander to change authoritatative storage format

--- linearmodels/panel/tests/test_data.py
+++ linearmodels/panel/tests/test_data.py
@@ -14,75 +14,65 @@ def seed():
 
 def test_numpy_3d(seed):
     n, t, k = 11, 7, 3
-    x = np.random.random((n, t, k))
+    x = np.random.random((k, t, n))
     dh = PanelDataHandler(x)
-    assert_equal(x, dh.a3d)
-    assert dh.nitems == n
+    assert_equal(np.reshape(x, (k, t, n)), dh.a3d)
+    assert dh.nentity == n
     assert dh.nobs == t
     assert dh.nvar == k
-    assert_equal(np.reshape(x, (n * t, k)), dh.a2d)
+    assert_equal(np.reshape(x.T, (n * t, k)), dh.a2d)
     items = ['entity.{0}'.format(i) for i in range(n)]
     obs = [i for i in range(t)]
     vars = ['x.{0}'.format(i) for i in range(k)]
-    expected = pd.Panel(x, items=items, major_axis=obs, minor_axis=vars)
+    expected = pd.Panel(np.reshape(x, (k, t, n)), items=vars,
+                        major_axis=obs, minor_axis=items)
     assert_panel_equal(dh.panel, expected)
-    expected_frame = expected.swapaxes(0, 1).swapaxes(0, 2).to_frame()
+    expected_frame = expected.swapaxes(1,2).to_frame()
     assert_frame_equal(dh.dataframe, expected_frame)
 
 
 def test_numpy_1d(seed):
     n, t, k = 11, 1, 1
     x = np.random.random(n)
-    dh = PanelDataHandler(x)
-    assert_equal(np.reshape(x, (n, t, k)), dh.a3d)
-    assert dh.nitems == n
-    assert dh.nobs == t
-    assert dh.nvar == k
-    assert_equal(np.reshape(x, (n * t, k)), dh.a2d)
-    items = ['entity.{0}'.format(i) for i in range(n)]
-    obs = [i for i in range(t)]
-    vars = ['x.{0}'.format(i) for i in range(k)]
-    expected = pd.Panel(np.reshape(x, (n, 1, 1)), items=items,
-                        major_axis=obs, minor_axis=vars)
-    assert_panel_equal(dh.panel, expected)
-    expected_frame = expected.swapaxes(0, 1).swapaxes(0, 2).to_frame()
-    assert_frame_equal(dh.dataframe, expected_frame)
+    with pytest.raises(ValueError):
+        PanelDataHandler(x)
 
 
 def test_numpy_2d(seed):
     n, t, k = 11, 7, 1
-    x = np.random.random((n, t))
+    x = np.random.random((t, n))
     dh = PanelDataHandler(x)
-    assert_equal(np.reshape(x, (n, t, k)), dh.a3d)
-    assert dh.nitems == n
+    assert_equal(np.reshape(x, (k, t, n)), dh.a3d)
+    assert dh.nentity == n
     assert dh.nobs == t
     assert dh.nvar == k
-    assert_equal(np.reshape(x, (n * t, k)), dh.a2d)
+    assert_equal(np.reshape(x.T, (n * t, k)), dh.a2d)
     items = ['entity.{0}'.format(i) for i in range(n)]
     obs = [i for i in range(t)]
     vars = ['x.{0}'.format(i) for i in range(k)]
-    expected = pd.Panel(np.reshape(x, (n, t, k)), items=items,
-                        major_axis=obs, minor_axis=vars)
+    expected = pd.Panel(np.reshape(x, (k, t, n)), items=vars,
+                        major_axis=obs, minor_axis=items)
     assert_panel_equal(dh.panel, expected)
-    expected_frame = expected.swapaxes(0, 1).swapaxes(0, 2).to_frame()
+    expected_frame = expected.swapaxes(1,2).to_frame()
     assert_frame_equal(dh.dataframe, expected_frame)
 
 
 def test_pandas_panel(seed):
     n, t, k = 11, 7, 3
-    x = np.random.random((n, t, k))
+    x = np.random.random((k, t, n))
     major = pd.date_range('12-31-1999', periods=7)
-    minor = ['var.{0}'.format(i) for i in range(1, k + 1)]
-    items = ['item.{0}'.format(i) for i in range(1, n + 1)]
+    items = ['var.{0}'.format(i) for i in range(1, k + 1)]
+    minor = ['entities.{0}'.format(i) for i in range(1, n + 1)]
     x = pd.Panel(x, items=items, major_axis=major, minor_axis=minor)
     dh = PanelDataHandler(x)
-    assert dh.nitems == n
+    assert dh.nentity == n
     assert dh.nobs == t
     assert dh.nvar == k
     assert_equal(dh.a3d, x.values)
-    assert_equal(dh.a2d, np.reshape(x.values, (n * t, k)))
+    expected = np.reshape(x.swapaxes(0,2).values, (n * t, k))
+    assert_equal(dh.a2d, expected)
     assert_panel_equal(x, dh.panel)
-    expected_frame = x.swapaxes(0, 1).swapaxes(0, 2).to_frame()
+    expected_frame = x.swapaxes(1,2).to_frame()
     assert_frame_equal(dh.dataframe, expected_frame)
 
 
