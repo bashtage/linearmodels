3b04dd57cb9ae904ef7927921a2309ffb0bd1393
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-04-19 11:35:53 +0100

    ENH: Check categoricals for nesting
    Check other effects for being nested within each other
    Ensure time index is datetime or numeric

--- linearmodels/compat/pandas.py
+++ linearmodels/compat/pandas.py
@@ -1,11 +1,11 @@
 try:
     from pandas.api.types import (is_string_like, is_numeric_dtype,
                                   is_categorical, is_string_dtype,
-                                  is_categorical_dtype)
+                                  is_categorical_dtype, is_datetime64_any_dtype)
 except ImportError:
     from pandas.core.common import (is_string_dtype, is_string_like,
                                     is_numeric_dtype, is_categorical,
-                                    is_categorical_dtype)
+                                    is_categorical_dtype, is_datetime64_any_dtype)
 
 __all__ = ['is_string_dtype', 'is_numeric_dtype', 'is_categorical',
            'is_string_like', 'is_categorical_dtype']
