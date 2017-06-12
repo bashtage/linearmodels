b01852ce6f47640ad01d7cf85fd8333d7f375121
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-06-12 10:49:28 +0100

    ENH: Add Munnell dataset for examples
    Add Munnell dataset for system examples

---
+++ linearmodels/datasets/munnell/__init__.py
@@ -0,0 +1,21 @@
+DESCR = """
+Munnell, A. “Why Has Productivity Declined? Productivity and Public 
+Investment.” New England Economic Review, 1990, pp. 3–22.
+
+STATE   Full State Name 
+ST_ABB  State Code   
+YR      Year
+P_CAP   Public Capital
+HWY     Highway Capital
+WATER   Water Utility Capital    
+UTIL    Utility Capital
+PC      Private Capital
+GSP     Gross state product  
+EMP     Employment (labor) 
+UNEMP   Unemployment rate
+"""
+
+
+def load():
+    from linearmodels import datasets
+    return datasets.load(__file__, 'munnell.csv.bz2')
