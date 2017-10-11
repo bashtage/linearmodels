d85af6ae1f67384164d8b47096b55a7c93470abc
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-10-11 12:30:00 +0100

    ENH: Add additional model to rool import
    Add SUR and OLS to root import

--- linearmodels/__init__.py
+++ linearmodels/__init__.py
@@ -1,20 +1,25 @@
 import os
 
-from linearmodels.asset_pricing.model import LinearFactorModel, LinearFactorModelGMM, \
-    TradedFactorModel
-from linearmodels.iv.model import IV2SLS, IVGMM, IVGMMCUE, IVLIML
-from linearmodels.panel.model import BetweenOLS, FirstDifferenceOLS, PanelOLS, PooledOLS, \
-    RandomEffects, FamaMacBeth
+from linearmodels.asset_pricing.model import (LinearFactorModel,
+                                              LinearFactorModelGMM,
+                                              TradedFactorModel)
+from linearmodels.iv.model import IV2SLS, IVGMM, IVGMMCUE, IVLIML, _OLS
+from linearmodels.panel.model import (BetweenOLS, FirstDifferenceOLS, PanelOLS,
+                                      PooledOLS, RandomEffects, FamaMacBeth)
+from linearmodels.system import SUR
 from ._version import get_versions
 
+OLS = _OLS
 WARN_ON_MISSING = os.environ.get('LINEARMODELS_WARN_ON_MISSING', True)
 WARN_ON_MISSING = False if WARN_ON_MISSING in ('0', 'False') else True
 DROP_MISSING = os.environ.get('LINEARMODELS_DROP_MISSING', True)
 DROP_MISSING = False if DROP_MISSING in ('0', 'False') else True
 
-__all__ = ['PooledOLS', 'PanelOLS', 'FirstDifferenceOLS', 'BetweenOLS', 'RandomEffects',
+__all__ = ['PooledOLS', 'PanelOLS', 'FirstDifferenceOLS', 'BetweenOLS',
+           'RandomEffects',
            'FamaMacBeth',
-           'IVLIML', 'IVGMM', 'IVGMMCUE', 'IV2SLS',
+           'IVLIML', 'IVGMM', 'IVGMMCUE', 'IV2SLS', 'OLS',
+           'SUR',
            'LinearFactorModel', 'LinearFactorModelGMM', 'TradedFactorModel',
            'WARN_ON_MISSING', 'DROP_MISSING']
 
