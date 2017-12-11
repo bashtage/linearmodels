47a1214d1a8ff8f69d1f730ddd7859354ff7c9b0
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-12-11 16:46:54 +0000

    ENH: Allow env to be set for SystemFormulas
    Allow env to be changed for system formulas
    Add doc string for predict
    Test rare predict paths
    Test rare formula parser paths

--- linearmodels/tests/system/test_formulas.py
+++ linearmodels/tests/system/test_formulas.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from itertools import product
 
 import numpy as np
@@ -7,6 +8,7 @@ from pandas import Series, concat
 from linearmodels import SUR, IVSystemGMM, IV3SLS
 from linearmodels.compat.pandas import assert_series_equal, assert_frame_equal
 from linearmodels.formula import sur, iv_system_gmm, iv_3sls
+from linearmodels.system.model import SystemFormulaParser
 from linearmodels.tests.system._utility import generate_3sls_data_v2
 from linearmodels.utility import AttrDict
 
@@ -100,6 +102,20 @@ def test_predict_partial(config):
     pred2 = res.predict(data=joined, dataframe=True)
     assert_frame_equal(pred2[pred.columns], pred)
 
+    eqns = AttrDict()
+    for key in list(mod._equations.keys())[1:]:
+        eqns[key] = mod._equations[key]
+    final = list(mod._equations.keys())[0]
+    eqns[final] = {'exog': None, 'endog': None}
+    pred3 = res.predict(equations=eqns, dataframe=True)
+    assert_frame_equal(pred2[pred3.columns], pred3)
+
+    eqns = AttrDict()
+    for key in mod._equations:
+        eqns[key] = {k: v for k, v in mod._equations[key].items() if v.shape[1] > 0}
+    pred4 = res.predict(equations=eqns, dataframe=True)
+    assert_frame_equal(pred2, pred4)
+
 
 def test_invalid_predict(config):
     fmla, model, interface = config
@@ -110,3 +126,37 @@ def test_invalid_predict(config):
     res = mod.fit()
     with pytest.raises(ValueError):
         res.predict(data=joined, equations=mod._equations)
+
+
+def test_parser(config):
+    fmla, model, interface = config
+    parser = SystemFormulaParser(fmla, joined, eval_env=5)
+    orig_data = parser.data
+    assert isinstance(orig_data, OrderedDict)
+    assert parser.eval_env == 5
+
+    parser.eval_env = 4
+    assert parser.eval_env == 4
+    exog = parser.exog
+    dep = parser.dependent
+    endog = parser.endog
+    instr = parser.instruments
+    for key in orig_data:
+        eq = orig_data[key]
+        assert_frame_equal(exog[key], eq['exog'])
+        assert_frame_equal(dep[key], eq['dependent'])
+        assert_frame_equal(endog[key], eq['endog'])
+        assert_frame_equal(instr[key], eq['instruments'])
+
+    labels = parser.equation_labels
+    for label in labels:
+        assert label in orig_data
+    new_parser = SystemFormulaParser(parser.formula, joined, eval_env=5)
+
+    new_data = new_parser.data
+    for key in orig_data:
+        eq1 = orig_data[key]
+        eq2 = new_data[key]
+        for key in eq1:
+            if eq1[key] is not None:
+                assert_frame_equal(eq1[key], eq2[key])
