f97a56db74c03e689f09b4d7761b403d912fe77c
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-07-29 14:44:21 +0100

    TEST: Add OpenBLAS/conda
    Force use of OpenBLAS through conda
    Fix small failure due to pandas reduction operator inconsistency

--- ci/install-from-conda.sh
+++ ci/install-from-conda.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-conda create -n linearmodels-test python=${PYTHON} numpy=${NUMPY} scipy=${SCIPY} pandas=${PANDAS} statsmodels=${STATSMODELS} matplotlib seaborn
+
+export BLAS_CONFIG="mkl blas=*=mkl"
+if [[ ${OPENBLAS} == "1" ]]; then
+  export BLAS_CONFIG="nomkl blas=*=openblas"
+fi;
+conda create -n linearmodels-test python=${PYTHON} ${BLAS_CONFIG} numpy=${NUMPY} scipy=${SCIPY} pandas=${PANDAS} statsmodels=${STATSMODELS} matplotlib seaborn
 source activate linearmodels-test
 if [[ ! -z ${XARRAY} ]]; then conda install xarray=${XARRAY}; fi
 conda install --yes --quiet sphinx ipython jupyter nbconvert nbformat seaborn
