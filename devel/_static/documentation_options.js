f61acecf0152693b21190c495099589ee3639e9a
Travis CI User <travis@example.org>
2018-09-11 08:07:43 +0000

    Update docs after building Travis build 1075 of bashtage/linearmodels
    The docs were built from the branch 'master' against the commit
    d8997ad101e96a1a47dd1b9724f997a343cea4fe.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/linearmodels/jobs/427059609.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/linearmodels-test/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.9+3.g77007ba',
+    VERSION: '4.9+5.gd8997ad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
