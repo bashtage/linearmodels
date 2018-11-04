347b4063913c525153b0e50e21b4c0102f8485fe
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-11-04 21:37:35 +0000

    BLD: Simplify install steps to pull out common
    Refactor travis config to pull out common

---
+++ ci/install-common.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+pip install cached_property codecov coverage doctr flake8 guzzle_sphinx_theme ipython jupyter nbconvert!=5.4 nbformat nbsphinx pytest pytest-cov pytest-xdist sphinx -q
--- ci/install-from-conda.sh
+++ ci/install-from-conda.sh
@@ -4,9 +4,7 @@ export BLAS_CONFIG="mkl blas=*=mkl"
 if [[ ${OPENBLAS} == "1" ]]; then
   export BLAS_CONFIG="nomkl blas=*=openblas"
 fi;
+conda update --all -q
 conda create -n linearmodels-test python=${PYTHON} ${BLAS_CONFIG} numpy=${NUMPY} scipy=${SCIPY} pandas=${PANDAS} statsmodels=${STATSMODELS} matplotlib seaborn
 source activate linearmodels-test
 if [[ ! -z ${XARRAY} ]]; then conda install xarray=${XARRAY}; fi
-conda install --yes --quiet sphinx ipython jupyter nbconvert nbformat seaborn
-pip install pytest pytest-xdist coverage pytest-cov codecov sphinx doctr nbsphinx guzzle_sphinx_theme cached_property -q
-pip install --upgrade --no-deps sphinx
