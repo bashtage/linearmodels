ce96e5b22c9593f90b8687a5605058a81aba9271
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-08-13 05:49:36 -0500

    MAINT: Remove pd.Panel
    Remove Panel which has been deprecated

--- ci/install-common.sh
+++ ci/install-common.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pip install cached_property codecov coverage doctr flake8 guzzle_sphinx_theme ipython jupyter nbconvert!=5.4 nbformat nbsphinx pytest pytest-cov pytest-xdist sphinx sphinx-autodoc-typehints -q
+pip install cached_property codecov coverage doctr flake8 guzzle_sphinx_theme ipython jupyter nbconvert nbformat nbsphinx pytest pytest-cov pytest-xdist sphinx sphinx-autodoc-typehints -q
