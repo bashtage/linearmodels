1e0b7d798c77c19600683fb77c8358dd4f8f6aa6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-09-13 23:41:59 +0000

    Update docs after building Travis build 1365 of bashtage/linearmodels
    The docs were built from the branch 'master' against the commit
    9ec20e071808768ccfc547e4dec76a40c94debb9.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/linearmodels/jobs/584812277.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/linearmodels-test/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.13+8.g8ef9464',
+    VERSION: '4.13+9.g9ec20e0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
