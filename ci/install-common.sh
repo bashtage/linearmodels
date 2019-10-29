7f477d5b7b8ec7e31c1c8f06f4dbaa3563563c92
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-10-29 16:26:48 +0000

    BLD: Verify wheel contains data files
    Add a test from a build wheel

--- ci/install-common.sh
+++ ci/install-common.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pip install property_cached codecov coverage doctr flake8 ipython jupyter nbconvert nbformat nbsphinx pytest pytest-cov pytest-xdist sphinx sphinx-autodoc-typehints
+pip install property_cached codecov coverage doctr flake8 ipython jupyter nbconvert nbformat nbsphinx "pytest>=5" pytest-cov pytest-xdist sphinx sphinx-autodoc-typehints
--- ci/install-from-pypi.sh
+++ ci/install-from-pypi.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-conda update --all -q
-conda create -n linearmodels-test python
-source activate linearmodels-test
-pip install numpy scipy pandas matplotlib matplotlib seaborn cython statsmodels
+if [[ ! -z ${NUMPY} ]]; then pip install numpy==${NUMPY}; else pip install numpy; fi
+pip install scipy pandas matplotlib matplotlib seaborn cython statsmodels
 if [[ ! -z ${XARRAY} ]]; then pip install xarray==${XARRAY}; fi
 if [[ ! -z ${PIP_PACKAGES} ]]; then pip install ${PIP_PACKAGES}; fi
