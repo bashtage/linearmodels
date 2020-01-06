05c5948303cba839b7749a798dde1041793c7273
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-01-06 18:29:58 +0000

    MAINT: Update requirements
    Update requirements inline with NEP-29
    Increase Python to 3.6

--- ci/install-common.sh
+++ ci/install-common.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pip install property_cached codecov coverage doctr flake8 ipython jupyter nbconvert nbformat nbsphinx "pytest>=5" pytest-cov pytest-xdist sphinx sphinx-autodoc-typehints
+pip install property_cached codecov coverage doctr flake8 ipython jupyter nbconvert nbsphinx nbformat "pytest>=5" pytest-cov "pytest-xdist<1.30" sphinx sphinx-autodoc-typehints
