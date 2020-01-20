534ad9a97075f1627c637b753bd0f46dae29ffa6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-20 18:49:37 +0000

    Update docs after building Travis build 1473 of bashtage/linearmodels
    The docs were built from the branch 'master' against the commit
    18f2ba41a6ed15492908671fb8b7a7d1c4fc0e9c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/linearmodels/jobs/639603629.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.5/bin/doctr deploy devel --build-tags

---
+++ devel/_sources/panel/panel/linearmodels.panel.covariance.FamaMacBethCovariance.DEFAULT_KERNEL.rst.txt
@@ -0,0 +1,8 @@
+:orphan:
+
+linearmodels.panel.covariance.FamaMacBethCovariance.DEFAULT\_KERNEL
+===================================================================
+
+.. currentmodule:: linearmodels.panel.covariance
+
+.. autoattribute:: FamaMacBethCovariance.DEFAULT_KERNEL
--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.15.4+3.g8f4353e',
+    VERSION: '4.15.4+9.g18f2ba4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
