717e2218f79964f9ce7f83eb129f561728d2f6ef
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-07-31 10:10:01 +0000

    Update docs after building Travis build 1642 of bashtage/linearmodels
    The docs were built from the branch 'master' against the commit
    7705cacb4a7a4aafafbf8b9a1c5d7a5d080f3e95.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/bashtage/linearmodels/jobs/366998493.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.6/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.17+24.g796e262',
+    VERSION: '4.17+27.g7705cac',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
