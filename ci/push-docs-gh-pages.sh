1578b78c6f6d82781589dff613a02d978d24afeb
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-12-14 16:05:03 +0000

    Try and fix action

--- ci/push-docs-gh-pages.sh
+++ ci/push-docs-gh-pages.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Set git email and name"
+git config user.email "kevin.k.sheppard@gmail.com"
+git config user.name "Kevin Sheppard"
 echo "Checkout pages"
 git checkout gh-pages
 echo "Remove devel"
@@ -12,8 +15,6 @@ echo "Add devel"
 git add devel/.
 echo "Change remove"
 git remote set-url origin https://bashtage:"${GH_PAGES_TOKEN}"@github.com/bashtage/linearmodels.git
-git config --global user.email "kevin.k.sheppard@gmail.com"
-git config --global user.name "Kevin Sheppard"
 echo "Github Actions doc build after commit ${GITHUB_REF##*/}"
 git commit -a -m "Github Actions doc build after commit ${GITHUB_REF##*/}"
 echo "Push"
