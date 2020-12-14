f5d9cd924b330584d01214510eb2b45346801964
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-12-14 09:07:56 +0000

    MAINT: Update Azure to allow specific packages
    Update to allow specific package testing on Linux
    Keep Windows generic

---
+++ ci/install-posix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CMD="python -m pip install numpy"
+if [[ -n ${NUMPY} ]]; then CMD="$CMD==${NUMPY}"; fi;
+CMD="$CMD scipy"
+if [[ -n ${SCIPY} ]]; then CMD="$CMD==${SCIPY}"; fi;
+CMD="$CMD pandas"
+if [[ -n ${PANDAS} ]]; then CMD="$CMD==${PANDAS}"; fi;
+CMD="$CMD statsmodels"
+if [[ -n ${STATSMODELS} ]]; then CMD="$CMD==${STATSMODELS}"; fi
+CMD="$CMD xarray"
+if [[ -n ${XARRAY} ]]; then CMD="$CMD==${XARRAY}"; fi
+echo $CMD
+eval $CMD
