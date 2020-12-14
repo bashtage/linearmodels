b83df6de9b2ba6c3b8babc89c2986aa7c7349727
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-12-14 16:30:24 +0000

    Update GitHub Actions to push on tagged branch

--- ci/push-docs-gh-pages.sh
+++ ci/push-docs-gh-pages.sh
@@ -9,16 +9,27 @@ echo "Remove devel"
 rm -rf devel
 echo "Make a new devel"
 mkdir devel
-echo "Move docs"
-mv doc/build/html/* devel/
+echo "Checking for tag"
+GIT_TAG=$(git name-rev --name-only --tags HEAD)
+if [[ ${GIT_TAG} == "undefined" ]]; then
+  echo "Tag is ${GIT_TAG}. Not updating main documents"
+else
+  echo "Tag ${GIT_TAG} is defined"
+  echo "Copy docs to root"
+  echo cp -R ${PWD}/doc/build/html/ ${PWD}/
+  cp -R ${PWD}/doc/build/html/* ${PWD}
+fi
+echo "Copy docs to devel"
+echo cp -R ${PWD}/doc/build/html/ ${PWD}/devel/
+cp cp -R ${PWD}/doc/build/html/ ${PWD}/devel/
 echo "Add devel"
 git add devel/.
-echo "Change remove"
+
+echo "Change remote"
 git remote set-url origin https://bashtage:"${GH_PAGES_TOKEN}"@github.com/bashtage/linearmodels.git
 echo "Github Actions doc build after commit ${GITHUB_REF##*/}"
 git commit -a -m "Github Actions doc build after commit ${GITHUB_REF##*/}"
 echo "Push"
 git push -f
-
 
 
