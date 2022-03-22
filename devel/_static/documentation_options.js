affe6d9ce9656a1ed3c4b9f55359917cfcccfd84
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-03-22 00:22:28 +0000

    Github Actions doc build after commit 23f8829b

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v4.25+33.gc6d5bf383',
+    VERSION: '4.26.dev35+g23f8829bf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
