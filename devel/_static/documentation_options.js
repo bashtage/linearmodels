8b5acba70d8d5304ef0879a26a10d241facf15ff
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-06-07 07:44:57 +0000

    Github Actions doc build after commit bf6a1400

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.27.dev6+gb01678ea3',
-    LANGUAGE: 'None',
+    VERSION: '4.27.dev8+gbf6a14001',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    ENABLE_SEARCH_SHORTCUTS: false,
 };
