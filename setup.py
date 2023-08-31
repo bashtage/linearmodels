cdeb8fcc67d3e9a387b42f17b3c0a59250ae4405
William Nelson <william.roy.nelson@gmail.com>
2023-08-31 10:51:22 -0700

    MAINT: Update License Classifier

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ def run_setup(binary: bool = True) -> None:
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
-            "License :: OSI Approved",
+            "License :: OSI Approved :: University of Illinois/NCSA Open Source License",
             "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: POSIX",
