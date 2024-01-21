60f9fa1e0e5c873fbc50aee3adbf61ab62e45bfd
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2024-01-21 16:24:18 +0000

    Refine CI configs

--- ci/install-posix.sh
+++ ci/install-posix.sh
@@ -11,12 +11,12 @@ if [[ -n ${STATSMODELS} ]]; then CMD="$CMD~=${STATSMODELS}"; fi
 if [[ -n ${XARRAY} ]]; then CMD="$CMD xarray~=${XARRAY}"; fi
 if [[ -n ${FORMULAIC} ]]; then CMD="$CMD formulaic~=${FORMULAIC}"; fi
 if [[ -n ${XXHASH} ]]; then CMD="$CMD xxhash"; fi
+if [[ -n ${PYARROW} ]]; then CMD="$CMD pyarrow"; fi
 echo "$CMD"
 eval "$CMD"
 
 if [ "${PIP_PRE}" = true ]; then
-  python -m pip uninstall -y numpy pandas scipy matplotlib statsmodels
-  python -m pip install pyarrow
-  python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy pandas scipy matplotlib --upgrade --use-deprecated=legacy-resolver
+  python -m pip uninstall -y numpy pandas scipy matplotlib statsmodels xarray
+  python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy pandas scipy matplotlib xarray --upgrade --use-deprecated=legacy-resolver
   python -m pip install git+https://github.com/statsmodels/statsmodels.git --upgrade --no-build-isolation -v
 fi
